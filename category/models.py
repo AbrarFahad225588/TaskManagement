221feeaf4a4d6555b598b6be2154063a773fd715
fahad <md9885397@gmail.com>
2024-08-10 21:02:27 +0600

    assignment code

---
+++ category/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class TaskCategory(models.Model):
+    category_name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.category_name
---
+++ task/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import TaskModel
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = TaskModel
+        fields = '__all__'
+        widgets={
+                
+            'task_assign_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'categories':  forms.CheckboxSelectMultiple
+            }
